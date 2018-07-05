7041e62c99c276ba286b7861563527b760c36e75
Eliasz Slusarczyk <eliasz@ergodicity.io>
2018-07-05 19:48:17 +0100

    add the generation of the setup.py file to the build.sh script

--- build.sh
+++ build.sh
@@ -2,4 +2,18 @@ make PROTOINCLUDE=$GOPATH/src/github.com/googleapis/googleapis LANGUAGE=go OUTPU
 make PROTOINCLUDE=$GOPATH/src/github.com/googleapis/googleapis LANGUAGE=python OUTPUT=.python/
 find .python/ -type f -name \*.go -delete
 
+cat > .python/setup.py << EOF
+from setuptools import setup, find_packages
 
+setup(
+    name='erego',
+    version='',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='eliaszs',
+    author_email='eliasz@ergodicity.io',
+    description='',
+    install_requires=[],
+)
+EOF
