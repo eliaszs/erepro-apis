311b7a9c50797afcfb5456748b64b7dfbeb38b90
Eliasz Slusarczyk <eliasz@ergodicity.io>
2018-07-05 19:50:11 +0100

    commit forgotten python package

---
+++ .python/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='erego',
+    version='',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='eliaszs',
+    author_email='eliasz@ergodicity.io',
+    description='',
+    install_requires=[],
+)
